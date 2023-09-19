7a8fe0cb0b98d9ad1c89f02af2463cd7093e2a3d
gurtaj56 <144941574+gurtaj56@users.noreply.github.com>
2023-09-19 13:23:43 -0400

    Create arithmetic_operator.java

---
+++ arithmetic_operator.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int a,b;  //declration 
+    a=6;  //initialization
+      b=3;
+      //int x = a%b;//modulo operator (remainder)
+    int x=a/b;
+    // int x=a/b;
+    double y=a/b;
+    //System.out.println("Integer value is"+x);
+    System.out.println(a<b);
+  }
+}
